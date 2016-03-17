f6fd99cb56f1e799c8ba9b265fd7875d4b8aae81
Mike Nolan <me@nolski.rocks>
2016-03-17 13:13:57 -0400

    Finished SJ updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,100 +1,104 @@
 /*
-	Elevation by Pixelarity
-	pixelarity.com @pixelarity
-	License: pixelarity.com/license
+  Elevation by Pixelarity
+  pixelarity.com @pixelarity
+  License: pixelarity.com/license
 */
 
-(function($) {
+(function ($) {
 
-	skel.breakpoints({
-		wide: '(max-width: 1680px)',
-		normal: '(max-width: 1280px)',
-		narrow: '(max-width: 980px)',
-		narrower: '(max-width: 840px)',
-		mobile: '(max-width: 736px)',
-		mobilep: '(max-width: 480px)'
-	});
+  skel.breakpoints({
+    wide: '(max-width: 1680px)',
+    normal: '(max-width: 1280px)',
+    narrow: '(max-width: 980px)',
+    narrower: '(max-width: 840px)',
+    mobile: '(max-width: 736px)',
+    mobilep: '(max-width: 480px)'
+  });
 
-	$(function() {
+  $(function () {
 
-		var	$window = $(window),
-			$body = $('body'),
-			$header = $('#header'),
-			$banner = $('#banner');
+    var  $window = $(window);
+    var $body = $('body');
+    var $header = $('#header');
+    var $banner = $('#banner');
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+    // Fix: Placeholder polyfill.
+    $('form').placeholder();
 
-		// Prioritize "important" elements on narrower.
-			skel.on('+narrower -narrower', function() {
-				$.prioritize(
-					'.important\\28 narrower\\29',
-					skel.breakpoint('narrower').active
-				);
-			});
+    // Prioritize "important" elements on narrower.
+    skel.on('+narrower -narrower', function() {
+        $.prioritize(
+          '.important\\28 narrower\\29',
+          skel.breakpoint('narrower').active
+        );
+      });
 
-		// Dropdowns.
-			$('#nav > ul').dropotron({
-				alignment: 'center'
-			});
+    // Dropdowns.
+    $('#nav > ul').dropotron({
+      alignment: 'center',
+    });
 
-		// Off-Canvas Navigation.
+    $('#header > ul').dropotron({
+      alignment: 'center',
+    });
 
-			// Navigation Button.
-				$(
-					'<div id="navButton">' +
-						'<a href="#navPanel" class="toggle"></a>' +
-					'</div>'
-				)
-					.appendTo($body);
+    // Off-Canvas Navigation.
 
-			// Navigation Panel.
-				$(
-					'<div id="navPanel">' +
-						'<nav>' +
-							$('#nav').navList() +
-						'</nav>' +
-					'</div>'
-				)
-					.appendTo($body)
-					.panel({
-						delay: 500,
-						hideOnClick: true,
-						hideOnSwipe: true,
-						resetScroll: true,
-						resetForms: true,
-						side: 'left',
-						target: $body,
-						visibleClass: 'navPanel-visible'
-					});
+    // Navigation Button.
+    $(
+      '<div id="navButton">' +
+      '<a href="#navPanel" class="toggle"></a>' +
+      '</div>'
+    )
+          .appendTo($body);
 
-			// Fix: Remove transitions on WP<10 (poor/buggy performance).
-				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-					$('#navButton, #navPanel, #page-wrapper')
-						.css('transition', 'none');
+    // Navigation Panel.
+    $(
+      '<div id="navPanel">' +
+        '<nav>' +
+          $('#nav').navList() +
+        '</nav>' +
+      '</div>'
+    )
+    .appendTo($body)
+    .panel({
+      delay: 500,
+      hideOnClick: true,
+      hideOnSwipe: true,
+      resetScroll: true,
+      resetForms: true,
+      side: 'left',
+      target: $body,
+      visibleClass: 'navPanel-visible'
+    });
 
-		// Header.
-		// If the header is using "alt" styling and #banner is present, use scrollwatch
-		// to revert it back to normal styling once the user scrolls past the banner.
-		// Note: This is disabled on mobile devices.
-			if (!skel.vars.mobile
-			&&	$header.hasClass('alt')
-			&&	$banner.length > 0) {
+    // Fix: Remove transitions on WP<10 (poor/buggy performance).
+    if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+    $('#navButton, #navPanel, #page-wrapper')
+      .css('transition', 'none');
 
-				$window.on('load', function() {
+    // Header.
+    // If the header is using "alt" styling and #banner is present, use scrollwatch
+    // to revert it back to normal styling once the user scrolls past the banner.
+    // Note: This is disabled on mobile devices.
+    if (!skel.vars.mobile
+    &&  $header.hasClass('alt')
+    &&  $banner.length > 0) {
 
-					$banner.scrollwatch({
-						delay:		0,
-						range:		1,
-						anchor:		'top',
-						on:			function() { $header.addClass('alt reveal'); },
-						off:		function() { $header.removeClass('alt'); }
-					});
+      $window.on('load', function () {
 
-				});
+        $banner.scrollwatch({
+          delay:    0,
+          range:    1,
+          anchor:    'top',
+          on:      function () { $header.addClass('alt reveal'); },
+          off:    function () { $header.removeClass('alt'); },
+        });
 
-			}
+      });
 
-	});
+    }
+
+  });
 
 })(jQuery);
